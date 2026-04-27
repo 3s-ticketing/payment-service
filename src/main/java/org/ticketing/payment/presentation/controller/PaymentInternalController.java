557f4d35570a5eaa5806f7b22d6ddf871e5b2826
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-04-27 20:25:10 +0900

    Feat: internal controller 추가

---
+++ src/main/java/org/ticketing/payment/presentation/controller/PaymentInternalController.java
@@ -0,0 +1,46 @@
+package org.ticketing.payment.presentation.controller;
+
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ticketing.payment.application.service.PaymentService;
+import org.ticketing.payment.presentation.dto.request.CreatePaymentRequestDto;
+import org.ticketing.payment.presentation.dto.request.PaymentSuccessRequestDto;
+import org.ticketing.payment.presentation.dto.response.PaymentResponseDto;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/internal/payments")
+@RequiredArgsConstructor
+public class PaymentInternalController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public PaymentResponseDto createPayment(@RequestBody @Valid CreatePaymentRequestDto request) {
+        return PaymentResponseDto.from(paymentService.createPayment(request.toCommand()));
+    }
+
+    @GetMapping("/{paymentId}")
+    public PaymentResponseDto getPayment(@PathVariable @NotNull UUID paymentId) {
+        return PaymentResponseDto.from(paymentService.getPayment(paymentId));
+    }
+
+    @GetMapping
+    public Page<PaymentResponseDto> getPayments(
+            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable) {
+        return paymentService.getPayments(pageable).map(PaymentResponseDto::from);
+    }
+}
